5298c7a686bb1242900c7be89c1ea50ca43f028c
thalesreistech <thalesreistech@gmail.com>
2026-07-19 22:40:46 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Relatorio-Pericia-de-credito/',
+    base: '/PericiaDeCredito/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
